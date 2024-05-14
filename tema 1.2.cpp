c2da75fd7c21144a206675e568cf48493d7a8437
Rolandos12 <163935309+Rolandos12@users.noreply.github.com>
2024-05-14 18:09:33 +0300

    Add files via upload
    an bisect si 5 factorial

---
+++ tema 1.2.cpp
@@ -0,0 +1,33 @@
+// tema 1.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// an bisect
+
+int main()
+{
+    int an, x;
+    cout << "an=";
+    cin >> an;
+    an = an % 2;
+
+    if (an != 0)
+
+        cout << "anul nu este bisect ";
+    else   cout << "anul este bisect";
+
+    return 0;
+}
+ // pt 5 factorial
+//#include <iostream>
+// using namespace std;
+
+// int main()
+//{
+  //  int x, n;
+  //  cout << "introduceti nr:";
+   // cin >> x;
+   // cout << "x factorial este:";
+   // x = x * (x - 1)(x - 2)(x - 3);
+   // cout << x;
+   // return 0;
+// }
